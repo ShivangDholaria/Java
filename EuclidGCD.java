ff9fc0760387405cbbd843649e49d804549dc996
Shivang Dholaria <dholariashivang@gmail.com>
2023-12-30 20:47:02 -0500

    GCD code

---
+++ EuclidGCD.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class EuclidGCD {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter 2 numbers");
+        int a = s.nextInt();
+        int b = s.nextInt();
+
+        while (b != 0) {
+            if(a > b)
+                a -=b;
+            else
+                b -= a;
+        }
+        System.out.println("GCD of the given 2 number is " + a);
+    }
+}
