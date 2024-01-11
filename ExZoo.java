8c1b630ae7e1aee3447e96eae0fec45d241d1fa1
Shivang Dholaria <dholariashivang@gmail.com>
2024-01-11 15:57:01 -0500

    Added different way of using the inner classes

--- ExZoo.java
+++ ExZoo.java
@@ -11,26 +11,44 @@ public class ExZoo {
     //Inner classes
     abstract class Animal {
         public abstract void speak();
+        public abstract String getSpecies();
     }
 
     //Subclass
     class Lion extends Animal {
+        
         public void speak() {
             System.out.print("roar");
+        }
+
+        //Ex 3
+        //Added new method to get species of the animal subclass
+        public String getSpecies() {
+            return "lion";
         }
     }
 
     class Gibbon extends Animal {
+        
         public void speak() {
             System.out.print("hoot");
+        }
+
+        public String getSpecies() {
+            return "gibbon";
         }
     }
 
     // Ex 2
     // New Class added
     class Mouse extends Animal {
+        
         public void speak() {
             System.out.print("squeak");
+        }
+
+        public String getSpecies() {
+            return "mouse";
         }
     }
 
@@ -54,6 +72,19 @@ public class ExZoo {
         System.out.print("The mouse goes \"");
         mouse.speak();
         System.out.println("\"");
+    
+        System.out.println("\n");
+        // Created animal array which stores the objects of its subclasses
+        // As subtypes can inherit its supertype
+        Animal[] myAnimals = { new Lion(), new Gibbon(), new Mouse() }; 
+        
+        for (Animal animal : myAnimals) {
+            System.out.print("The ");
+            System.out.print(animal.getSpecies());
+            System.out.print(" goes \"");
+            animal.speak();
+            System.out.println("\"");
+        }
     }
 
     public static void main(String args[]) {
