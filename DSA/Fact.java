b6398953e31f4fe865cdcb8f6525902b14416dbc
Shivang Dholaria <dholariashivang@gmail.com>
2024-01-16 22:11:50 -0500

    Factorial code using recursion

---
+++ DSA/Fact.java
@@ -0,0 +1,20 @@
+//package DSA;
+
+
+public class Fact {
+    public long fact(long num) {
+        if(num <= 1)
+            return 1;
+        return num * fact(num - 1);
+    }
+    
+    public static void main(String[] args) {
+        Fact f = new Fact();
+        
+        int counter = 10;
+
+        for (int i = 0; i < counter; i++) {
+            System.out.println("Factorial of " + i + " is " + f.fact(i));
+        }
+    }
+}
