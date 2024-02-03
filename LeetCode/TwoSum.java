f17e56ded011098f18383cdfa3d9ad25236c2319
Shivang Dholaria <dholariashivang@gmail.com>
2024-02-03 18:44:41 -0500

    Added more Solutions to the TwoSum problem

--- LeetCode/TwoSum.java
+++ LeetCode/TwoSum.java
@@ -1,24 +1,75 @@
 package LeetCode;
 
-import java.util.ArrayList;
+/*
+ * 
+ */
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class TwoSum {
     //1st sol
-    public static int[] twoSum(int[] nums, int target) {
-        int arr[];
-        ArrayList<Integer> ind = new ArrayList<Integer>();
-        for (int i = 0, k = 0; i < nums.length - 2; i++) {
-            for (int j = i + 1; j < nums.length - 1; j++) {
+    public static int[] twoSum_1(int[] nums, int target) {
+        int arr[] = new int[2];
+        for (int i = 0, k = 0; i < nums.length - 1 && k <= 2; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
                 if(nums[i] + nums[j] == target)
                 {
-                    ind.add(i);
-                    ind.add(j);
+                    arr[k++] = i;
+                    arr[k++] = j;
+                    break;
                 }
             }
         }
-        return arr.;
+        return arr;
+    }
+
+    //2nd sol - Kind of optimized version of 1st sol
+    public static int[] twoSum_2(int[] nums, int target) {
+        for (int i = 0, k = 0; i < nums.length - 1 && k <= 2; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target)
+                {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return new int[] {};
+    }
+
+    //3rd sol - using HashMap
+    public static int[] twoSum_3(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            numMap.put(nums[i], i);            
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int comp = target - nums[i];
+
+            if(numMap.containsKey(comp) && numMap.get(comp) != i)
+                return new int[] {i, numMap.get(comp)};
+        }
+
+        return new int[] {};
+    }
+
+    //4th sol
+    public static int[] twoSum_4(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int comp = target - nums[i];
+
+            if(numMap.containsKey(comp))
+                return new int[] {numMap.get(comp), i};
+            
+            numMap.put(nums[i], i);
+        }
+
+        return new int[] {};
     }
 
     public static void main(String[] args) {
@@ -36,6 +87,10 @@ public class TwoSum {
         System.out.println("Enter target sum");
         int sum = s.nextInt();
 
-        System.out.println("Indices of the given list whose sum = " + sum + " is " + Arrays.toString(twoSum(nums, sum)));
+        System.out.println("Indices of the given list whose sum = " + sum + " is " + Arrays.toString(twoSum_1(nums, sum)));
+        System.out.println("Indices of the given list whose sum = " + sum + " is " + Arrays.toString(twoSum_2(nums, sum)));
+        System.out.println("Indices of the given list whose sum = " + sum + " is " + Arrays.toString(twoSum_3(nums, sum)));
+        System.out.println("Indices of the given list whose sum = " + sum + " is " + Arrays.toString(twoSum_4(nums, sum)));
+        s.close();
     }
 }
